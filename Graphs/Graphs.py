4bf3068b211dae790da88c5d306cff9d41b226c7
Tom <tomhadlaw144@gmail.com>
2015-05-28 22:25:02 -0300

    Cleanup and combined graph objects.

--- Graphs/Graphs.py
+++ Graphs/Graphs.py
@@ -64,8 +64,7 @@ class DiGraph():
         return "<DiGraph %d>" % self.n
     def __repr__(self):
         return "<DiGraph %d>" % self.n
-
-class Graph():
+#class Graph():
     """Represents a general unweighted Graph"""
     V = [] #start with empty vertex set
     n = 0
@@ -95,24 +94,3 @@ class Graph():
         """Prints adjacency list for graph"""
         for i in range(0, self.n):
             print "V_" + str(i) + " ---> {" + str(self.Adj[i]) + "}"
-
-def importGraphFile(filename):
-    """Reads in graph data from file and returns a graph object.
-        Args:
-        param1 (filename): String of filename to read graph data from.
-        Returns:    Graph containing the read in edges.
-        Assumptions:First line in file is an integer representing the n = size
-        of the graph, all lines after that contain edges in the
-        form i,j.
-        Notes:      Ignores edge weights parameters.
-        """
-    f = open(filename, "r")
-    n = int(f.readline())
-    G = Graph(n)
-    sys.setrecursionlimit(n + 1)
-    count = 0
-    for line in f:
-        count += 1
-        arr = line.split(",")
-        G.addEdge(int(arr[0]) - 1, int(arr[1]) - 1)
-    return G
